21925e22d23e7a31d12919c0b9461c5b07a442c7
Harshit Purohit <103580436+purohitharshit@users.noreply.github.com>
2023-09-24 20:01:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="my-2 flex justify-center items-center gap-2  text-xl font-bold h-[60px] rounded-lg bg-white">
-      <img src="/logos_firebase.png" />
+      <img src="logos_firebase.png" />
       <h1>Firebase contact app</h1>
     </div>
   );
